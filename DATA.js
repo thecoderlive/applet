3fefdfa6740893f5c155958fb72b00819e4c9029
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-14 18:21:11 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,47 @@
+export const item = {
+    "id": "13dda4e3-33b4-428b-8314-c289235c824e",
+    "applet_list": [
+        {
+            "id": "c3d0bb7d-1c8e-421f-bd17-d969afbd7c54",
+            "applet_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/17.png",
+            "applet_for": "That dinner such enjoy day. Increase source finally dark. Five option assume follow bag data.",
+            "indicator": "second",
+            "works_with": "game",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/124.png"
+        },
+        {
+            "id": "7d253565-41ba-458c-a008-0775a85a3836",
+            "applet_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png",
+            "applet_for": "Drug long image line collection.",
+            "indicator": "design",
+            "works_with": "tree",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+        },
+        {
+            "id": "bc243d0a-cb6a-4df7-953d-be24cd1114f8",
+            "applet_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/53.png",
+            "applet_for": "She serious test everything other sister. Sense although full. If state they site bad above right.",
+            "indicator": "a",
+            "works_with": "after",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png"
+        },
+        {
+            "id": "2fa96ecd-6963-464b-9993-d8ceab8c9a02",
+            "applet_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/287.png",
+            "applet_for": "Eye world possible whether far condition. Fill window treat same us break note. Ground account his throughout newspaper first finally often.",
+            "indicator": "let",
+            "works_with": "civil",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/192.png"
+        },
+        {
+            "id": "528e4a73-a6cb-4e44-a7bb-fde359c9c913",
+            "applet_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png",
+            "applet_for": "Including beyond protect country second. Federal certainly else difference. President close remember scientist or affect.",
+            "indicator": "nation",
+            "works_with": "information",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png"
+        }
+    ],
+    "manage_applets": "Each admit far term through. Reflect we beautiful front according market.",
+    "about_applets": "Serve discuss subject throw leave fear item drug. Paper response full yourself far human would role. Fire several agent development walk idea finish."
+}
